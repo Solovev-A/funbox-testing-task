be5584016192585efd3835c80c28b6fa4e814b67
Александр Соловьев <solovev.off@gmail.com>
2022-06-16 22:34:40 +0300

    Добавлен фон страницы

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 import 'normalize.css';
 import './app.scss';
 
+
 export const App = () => {
     return (
-        <h1>Работает!</h1>
+        <>
+            <h1>Работает!</h1>
+        </>
     )
 }
